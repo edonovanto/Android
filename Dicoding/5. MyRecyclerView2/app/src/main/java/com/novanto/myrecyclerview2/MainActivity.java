110258e8401364b83f8b9d18d2985867c59c85da
Edo Novanto <novantoedo@gmail.com>
2019-10-21 23:49:19 +0700

    Card Mode Success

--- Dicoding/5. MyRecyclerView2/app/src/main/java/com/novanto/myrecyclerview2/MainActivity.java
+++ Dicoding/5. MyRecyclerView2/app/src/main/java/com/novanto/myrecyclerview2/MainActivity.java
@@ -14,12 +14,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvHeroes;
     private ArrayList<Hero> list = new ArrayList<>();
-
+    private String title = "Mode List";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setActionBarTitle(title);
 
         rvHeroes = findViewById(R.id.rv_heroes);
         rvHeroes.setHasFixedSize(true);
@@ -41,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
         rvHeroes.setAdapter(gridHeroAdapter);
     }
 
+    private void showRecyclerCardView(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewAdapter cardViewAdapter = new CardViewAdapter(list);
+        rvHeroes.setAdapter(cardViewAdapter);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main,menu);
@@ -53,19 +60,30 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void setActionBarTitle(String title){
+        if(getSupportActionBar() !=null){
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
     public void setMode(int selectedMode){
         switch (selectedMode){
             case R.id.action_list:
+                title = "Mode List";
                 showRecyclerList();
                 break;
 
             case R.id.action_grid:
+                title = "Mode Grid";
                 showRecyclerGrid();
                 break;
 
             case R.id.action_cardView:
+                title = "Mode Card";
+                showRecyclerCardView();
                 break;
         }
+        setActionBarTitle(title);
     }
 
 }
