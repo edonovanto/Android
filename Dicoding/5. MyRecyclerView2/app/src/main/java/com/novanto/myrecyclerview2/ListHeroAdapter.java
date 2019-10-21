5ceb4989fbacc32556560e311afde32de1f469c3
Edo Novanto <novantoedo@gmail.com>
2019-10-21 22:54:59 +0700

    List Success

---
+++ Dicoding/5. MyRecyclerView2/app/src/main/java/com/novanto/myrecyclerview2/ListHeroAdapter.java
@@ -0,0 +1,62 @@
+package com.novanto.myrecyclerview2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class ListHeroAdapter extends RecyclerView.Adapter<ListHeroAdapter.ListViewHolder> {
+    private ArrayList<Hero> listHero;
+
+    //buat objek bernama list hero
+    public ListHeroAdapter(ArrayList<Hero> list){
+        this.listHero = list;
+    }
+
+    //Menampilkan data ke bentuk tampilan item_row_hero.xml
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_row_hero,viewGroup,false);
+        return new ListViewHolder(view);
+    }
+
+
+    //set gambar,nama, dan detail pahlawan
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        Hero hero = listHero.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(hero.getPhoto())
+                .apply(new RequestOptions().override(55,55))
+                .into(holder.imgPhoto);
+        holder.tvName.setText(hero.getName());
+        holder.tvDetail.setText(hero.getDetail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listHero.size();
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        TextView tvName, tvDetail;
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+            tvName = itemView.findViewById(R.id.tv_item_name);
+            tvDetail = itemView.findViewById(R.id.tv_item_detail);
+        }
+    }
+}
--- Dicoding/5. MyRecyclerView2/app/src/main/java/com/novanto/myrecyclerview2/MainActivity.java
+++ Dicoding/5. MyRecyclerView2/app/src/main/java/com/novanto/myrecyclerview2/MainActivity.java
@@ -1,14 +1,62 @@
 package com.novanto.myrecyclerview2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        //panggil data yang telah dibuat
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
     }
+
+    private void showRecyclerList() {
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode){
+        switch (selectedMode){
+            case R.id.action_list:
+                break;
+
+            case R.id.action_grid:
+                break;
+
+            case R.id.action_cardView:
+                break;
+        }
+    }
+
 }
