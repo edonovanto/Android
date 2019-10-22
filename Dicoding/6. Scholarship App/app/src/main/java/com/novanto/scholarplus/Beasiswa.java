39182f12c5bed2a72303f3c52a4fc32c66d2ecdb
Edo Novanto <novantoedo@gmail.com>
2019-10-22 13:52:56 +0700

    List Data

---
+++ Dicoding/6. Scholarship App/app/src/main/java/com/novanto/scholarplus/Beasiswa.java
@@ -0,0 +1,38 @@
+package com.novanto.scholarplus;
+
+public class Beasiswa {
+    private String name,detail,syarat;
+    private int photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getSyarat() {
+        return syarat;
+    }
+
+    public void setSyarat(String syarat) {
+        this.syarat = syarat;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
---
+++ Dicoding/6. Scholarship App/app/src/main/java/com/novanto/scholarplus/DataBeasiswa.java
@@ -0,0 +1,102 @@
+package com.novanto.scholarplus;
+
+public class DataBeasiswa {
+    private static String[] beasiswaNames = {
+            "Peningkatan Prestasi Akademik",
+            "Bidikmisi",
+            "Adaro Foundation",
+            "Sakura Science",
+            "Glints",
+            "Bank Tabungan Negara",
+            "DataPrint",
+            "Digital Talent",
+            "Paragon",
+            "AMINEF FullBright"
+    };
+
+    private static String[] beasiswaDetails = {
+            "Beasiswa PPA 2019 dan BPP PPA 2019 ditujukan bagi mahasiswa aktif yang berada di jenjang S1/DIV atau mahasiswa D3. Untuk teknis beasiswa PPA 2019 - 2020 tidak jauh berbeda dengan periode sebelumnya, di mana pendaftaran ditetapkan oleh masing-masing perguruan tinggi. Penerima beasiswa dan bantuan biaya pendidikan PPA akan memperoleh dana sebesar Rp 400 ribu/bulan. Beasiswa tersebut diberikan untuk pertamakalinya di periode tahun angggaran berjalan sekurangnya selama 6 bulan. Pengajuan beasiswa PPA dan BPP-PPA cukup mudah karena mahasiswa bisa langsung mendaftar di kampus masing-masing.",
+            "Bidikmisi adalah bantuan biaya pendidikan dari pemerintah bagi lulusan Sekolah Menengah Atas (SMA) atau sederajat yang memiliki potensi akademik baik tetapi memiliki keterbatasan ekonomi.? Berbeda dari beasiswa yang berfokus pada memberikan penghargaan atau dukungan dana terhadap mereka yang berprestasi (lihat penjelasan Pasal 76 UU No. 12 Tahun 2012 tentang Pendidikan Tinggi). Walaupun demikian, syarat prestasi pada bidikmisi ditujukan untuk menjamin bahwa penerima bidikmisi terseleksi dari yang benar benar mempunyai potensi dan kemauan untuk menyelesaikan pendidikan tinggi.",
+            "Melanjutkan kerjasama yang sudah terjalin selama ini Yayasan Pelayanan Kasih A & A Rachmat tetap berkomitmen memberikan Beasiswa beberapa orang mahasiswa dari berbagai bidang ilmu sesuai dengan persyaratan. Sesuai dengan kebijakan Management Yayasan Pelayanan Kasih A & A Rachmat akan berkolaborasi dengan Adaro Foundation melalui PT. Adaro Indonesia yang didirikan oleh PT. Adaro Energy untuk memberikan Beasiswa Tahun Akademik 2018-2019 sebesar Rp 650.000/bulan yang dibayarkan setiap 3 bulan sekali. ",
+            "Program Ilmu Pengetahuan Sakura adalah kegiatan tahunan yang sepenuhnya didanai oleh Badan Sains dan Teknologi Jepang (JST) untuk meningkatkan kualitas ilmu pengetahuan dan teknologi di Jepang dan negara-negara Asia lainnya. Kegiatan yang dilakukan terdiri dari penelitian dan kunjungan lapangan. Pada tahun 2019, Program Sains Sakura diadakan di Lembaga Penelitian Gempa Bumi (ERI), Universitas Tokyo.",
+            "Beasiswa Glints merupakan program rutin dari Glints yang ditujukan untuk membantu mahasiswa dan mahasiswi di Indonesia dalam mengembangkan karier dan meningkatkan kemampuan sedini mungkin. Mahasiswa D3, D4 dan S1 dari seluruh perguruan tinggi di Indonesia bisa mendaftar beasiswa ini. Sebagai informasi, Glints merupakan sebuah platform rekrutmen nomer 1 di Asia yang membantu perusahaan dalam membangun tim sukses yang terdiri dari talenta muda. Glints Scholarship memberikan beasiswa dalam bentuk uang tunai senilai Rp 3.000.000,-. Bantuan dana tersebut bisa digunakan untuk membayar uang kuliah / kursus / tes bahasa / persiapan student exchange, dll. Selain itu beasiswa yang diberikan tidak memerlukan keterikatan lanjutan dengan Glints.",
+            "Bank BTN anak memberikan apresiasi kepada mahasiswa berprestasi  dengan memberikan beasiswa masing-masing sebesar Rp 6.000.000,- untuk mahasiswa baik S1 maupun D3.",
+            "Program beasiswa DataPrint telah memasuki tahun kesembilan. Setelah sukses mengadakan program beasiswa di tahun 2011 hingga 2018, maka DataPrint kembali membuat program beasiswa bagi penggunanya yang berstatus pelajar dan mahasiswa.  Untuk mengetahui varian produk DataPrint, kamu bisa mengunjungi website resmi DataPrint di  www.dataprint.co.id .\n" +
+                    "Di tahun 2019 sebanyak 450 beasiswa akan diberikan bagi pendaftar yang terseleksi. Program beasiswa dibagi dalam dua periode. Tidak ada sistem kuota berdasarkan daerah dan atau sekolah/perguruan tinggi. Hal ini bertujuan agar beasiswa dapat diterima secara merata bagi seluruh pengguna DataPrint.  Beasiswa terbagi dalam tiga nominal yaitu Rp 400 ribu, Rp 600 ribu dan Rp 1 juta. Dana beasiswa akan diberikan satu kali bagi peserta yang lolos penilaian. Aspek penilaian berdasarkan dari essay, prestasi dan keaktifan peserta.\n" +
+                    "Di tahun 2019 ini ada Merchandise DataPrint juga loh untuk 5 orang peserta yang paling banyak merekomendasikan program ini ke teman-teman. Jika nama lengkapmu dan asal sekolah atau kampus kamu tertera di formulir pendaftaran temanmu (nama pemberi rekomendasi), siap-siap deh kamu akan jadi pemenangnya di akhir periode\n",
+            "Kementerian Komunikasi dan Informatika membuka pendaftaran bagi calon penerima 25.000 beasiswa program pelatihan Digital Talent Scholarship 2019. Beasiswa itu ditujukan untuk meningkatkan keterampilan dan daya saing sumber daya manusia Indonesia di bidang teknologi informasi dan komunikasi. \n" +
+                    "Peserta program akan dilatih secara intensif untuk menguasai hardskill dan softskill sesuai dengan peminatan di bidang teknis Artifical Intelligence, Big Data, Cloud Computing, Cyber Security, Internet of Things, dan Machine Learning serta beberapa tema pelatihan lainnya. \n" +
+                    "Sebelumnya, di tahun 2018, Kementerian Kominfo telah meluncurkan program yang sama untuk menyiapkan 1.000 talenta yang menguasai keahlian digital guna mendukung visi Indonesia untuk menjadi negara ekonomi digital terbesar pada tahun 2030. Selain itu,  Program Digital Talent Scholarship ditujukan untuk menyediakan talenta yang dibutuhkan dalam Revolusi Industri 4.0.\n" +
+                    "Pada tahun ini, Program Digital Talent Scholarship 2019 akan memberikan kesempatan kepada 25.000 peserta untuk mengikuti pelatihan yang dikemas dalam empat akademi\n",
+            "Paragon Technology and Innovation saat ini menyelenggarakan PARAGON SCHOLARSHIP bagi mahasiswa S1 dari 7 universitas mitra penyelenggara. Sekilas mengenai PT Paragon Technology and Innovation, merupakan perusahaan yang bergerak di bidang industri kosmetik sejak tahun 1985. Bidang pendidikan menjadi salah satu fokus Paragon dalam menggerakkan kegiatan positif di masyarakat. Salah satunya dengan memberikan beasiswa bagi mahasiswa di Perguruan Tinggi. \n" + "DAFTAR PERGURUAN TINGGI YANG BEKERJASAMA DALAM PARAGON SCHOLARSHIP:\n" +
+                    "1.\tUniversitas Indonesia (UI)\n" +
+                    "2.\tUniversitas Gadjah Mada (UGM)\n" +
+                    "3.\tInstitut Teknologi Bandung (ITB)\n" +
+                    "4.\tUniversitas Diponegoro (UNDIP)\n" +
+                    "5.\tInstitut Pertanian Bogor (IPB)\n" +
+                    "6.\tUniversitas Brawijaya (UB)\n" +
+                    "7.\tUniversitas Padjajaran (UNPAD)\n" +
+                    "\n" +
+                    "CAKUPAN BEASISWA & KEUNTUNGAN:\n" +
+                    "1.\tBantuan dana hingga Rp 12juta selama 1 tahun\n" +
+                    "2.\tPelatihan pengembangan diri meliputi skill komunikasi, presentasi, dan kepemimpinan\n" +
+                    "3.\tPelaksanaan Community Project (Kampus)\n" +
+                    "4.\tMenambah relasi. Peserta akan bertemu dengan peserta lainnya dari berbagai kampus dan komunitas\n",
+            "Program CCI atau COMMUNITY COLLEGE INITIATIVE merupakan suatu program akademik non-gelar untuk menempuh bidang studi tertentu pada institusi penyelenggara di Amerika Serikat, dengan tujuan untuk membangun keterampilan teknis, meningkatkan kemampuan kepemimpinan, dan memperkuat kemampuan bahasa Inggris. Program ini juga memberikan kesempatan untuk melakukan magang profesional, layanan pembelajaran, dan keterlibatan dalam masyarakat.\n" +
+                    "Nantinya peserta akan menempuh satu tahun akademik di Amerika Serikat dan mendapatkan sertifikat akademik dari bidang yang dipilih. Diharapkan setelah masa program berakhir, peserta kembali ke negara asal dengan keterampilan dan keahlian yang telah didapatkan untuk berkontribusi dalam pertumbuhan ekonomi dan pembangunan di negara asal.\n"
+    };
+
+    private static String[] syaratBeasiswa = {
+            "1. Calon penerima adalah mahasiswa yang kuliah pada perguruan tinggi pengelola beasiswa dan bantuan biaya pendidikan PPA di lingkungan Kementerian Riset, Teknologi dan Pendidikan Tinggi\n" +
+                    "2. Calon penerima harus terdaftar pada Pangkalan Data Pendidikan Tinggi (PD-Dikti)\n" +
+                    "3. Calon penerima adalah mahasiswa yang masih aktif, dalam jenjang pendidikan Diploma dan Sarjana.\n",
+            "1. Siswa SMA/SMK/MA atau bentuk lain yang sederajat yang akan lulus pada tahun 2019, memiliki identitas berupa NISN dan NPSN yang valid di PDSPK.\n" +
+                    "2. Lulusan tahun 2018 yang bukan penerima Bidikmisi dan tidak bertentangan dengan ketentuan penerimaan mahasiswa baru di masing-masing perguruan tinggi;\n" +
+                    "3. Usia paling tinggi pada saat mendaftar adalah 21 tahun;\n" +
+                    "4. Tidak mampu secara ekonomi\n",
+            "1.\tMahasiswa/I berkebangsaan Indonesia\n" +
+                    "2.\tBidang/Jurusan:\n" +
+                    "o\tD4 & S1: Teknik, MIPA, Pertanian, Kehutanan, dan Kedokteran\n" +
+                    "o\tD3: Politeknik\n" +
+                    "3.\tMinimal Semester III atau tahun ajaran kedua\n" +
+                    "4.\tIndeks Prestasi Kumulatif (IPK) terakhir:\n" +
+                    "o\tProgram D4 & S1: 3.00\n" +
+                    "o\tProgram D3: 2.85\n",
+            "1.\tPeserta merupakan mahasiswa aktif Program Studi S1 Teknik Informatika UII angkatan 2015, 2016, atau 2017.\n" +
+                    "2.\tPeserta memiliki IPK >= 3.00.\n" +
+                    "3.\tPeserta belum pernah mengunjungi negara Jepang sebelumnya dan berkeinginan untuk melanjutkan studi/bekerja di Jepang setelah lulus kuliah S1.\n" +
+                    "4.\tPeserta bersedia mengurus dan menanggung biaya pembuatan passport dan visa jika terpilih untuk kunjungan ke Jepang.\n" +
+                    "5.\tPeserta menguasai satu atau lebih bahasa pemrograman komputer.\n" +
+                    "6.\tPeserta dapat berbahasa Inggris dengan lancar (dibuktikan dengan skor TOEFL/CEPT minimum 500).\n",
+            "1.\tMerupakan mahasiswa D3, D4 dan S1 dari kampus manapun di Indonesia.\n" +
+                    "2.\tTidak ada batasan usia atau tahun pendidikan.\n" +
+                    "3.\tGlints Scholarship memperbolehkan peserta yang telah mendapatkan beasiswa lainnya untuk mendaftar. Karena program ini bukan merupakan bentuk kerja sama dengan pihak manapun.\n",
+            "1.\tMahasiswa Sarjana dan Diploma semua jurusan\n" +
+                    "2.\tIPK semester Genap 2018 yaitu =00\n" +
+                    "3.\tTidak sedang menerima beasiswa atau ikatan dinas dari pihak manapun\n" +
+                    "4.\tDiutamakan berasal dari keluarga kurang mampu\n",
+            "1. Essay merupakan OPINI PRIBADI. Tuangkan ide kamu semenarik mungkin.\n" +
+                    "2. Penulisan dan tata bahasa sesuai dengan kaidah EYD.\n" +
+                    "3. Panjang tulisan minimal 100 kata, maksimal 500 kata.\n" +
+                    "4. Apabila pendaftar menyertakan kutipan atau data (milik orang lain / dari internet) tanpa menyertakan sumber/link, maka akan dianggap copy paste dan formulir akan didiskualifikasi oleh panitia.\n",
+            "1. Warga Negara Indonesia (WNI) \n" +
+                    "2. Usia maksimal 29 tahun \n" +
+                    "3. Scan KTP dan Pasfoto \n" +
+                    "4. Lulus pendidikan D3/S1 dengan IPK minimal 2,75 dan memiliki Surat Keterangan Lulus atau Ijazah dari institusi terkait \n" +
+                    "5. Direkomendasikan oleh dosen (atau atasan apabila telah bekerja) \n" +
+                    "6. Sehat jasmani dan rohani \n" +
+                    "7. Lolos seleksi administrasi dan tes pengetahuan dasar (yang dilakukan saat pendaftaran)\n",
+            "1.\tMerupakan mahasiswa tahun ketiga (semester 5 dan 6) pada universitas yang disebutkan di atas\n" +
+                    "2.\tMempunyai IPK ≥ 3,3 (dibuktikan dengan transkrip nilai akademik)\n" +
+                    "3.\tMembuat Essay “Kontribusiku untuk Indonesia”\n" +
+                    "4.\tMemiliki sertifikat prestasi akademik/non-akademik (tidak wajib)\n" +
+                    "5.\tTidak sedang mengikuti beasiswa lainnya (termasuk Bidikmisi)\n",
+            "1.\tMerupakan Warga Negara Indonesia dan bukan merupakan warga negara atau bertempat tinggal di Amerika Serikat.\n" +
+                    "2.\tMemiliki kualitas kepemimpinan dan menunjukkan pengalaman dalam hal layanan masyarakat.\n" +
+                    "3.\tMemiliki komitmen dan telah mempersiapkan diri pada bidang studi yang akan dipilih.\n" +
+                    "4.\tLancar berkomunikasi dalam bahasa Inggris.\n" +
+                    "5.\tMemiliki catatan akademik yang memuaskan.\n" +
+                    "6.\tBukan merupakan karyawan/anggota keluarga dari karyawan AMINEF/Kedutaan Besar AS/Departemen Luar Negeri AS.\n" +
+                    "7.\tTidak sedang menerima beasiswa lainnya pada saat pendaftaran beasiswa ini.\n"
+    };
+}
