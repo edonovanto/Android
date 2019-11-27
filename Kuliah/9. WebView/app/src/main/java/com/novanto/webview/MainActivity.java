c88207d624ea0c04dfb1da52d08a32093a5ef40e
Edo Novanto <novantoedo@gmail.com>
2019-11-27 09:48:27 +0700

    Web View

--- Kuliah/9. WebView/app/src/main/java/com/novanto/webview/MainActivity.java
+++ Kuliah/9. WebView/app/src/main/java/com/novanto/webview/MainActivity.java
@@ -3,6 +3,12 @@ package com.novanto.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final WebView webView = findViewById(R.id.webView);
+        EditText editText = findViewById(R.id.edtURL);
+        Button browse = findViewById(R.id.btnBrowse);
+
+//        final String url = editText.getText().toString().trim();
+        final String url = "https://google.com";
+
+        browse.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(url.isEmpty()){
+                    Toast.makeText(MainActivity.this,"URL Kosong", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    webView.loadUrl(url);
+                    webView.setWebViewClient(new MainActivity.WebViewClient());
+                }
+            }
+        });
+    }
+
+    public class WebViewClient extends android.webkit.WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            return false;
+        }
+        @Override
+        public void onPageFinished(WebView view, String url){
+
+        }
     }
 }
