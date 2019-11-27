c5e96bc687c202b8ec4f15e30588e46b51179d40
Edo Novanto <novantoedo@gmail.com>
2019-11-27 08:28:15 +0700

    SQLite

---
+++ Kuliah/8. SQLite/app/src/main/java/com/komsi/sqlite/ContactModel.java
@@ -0,0 +1,29 @@
+package com.komsi.sqlite;
+
+public class ContactModel {
+    private int id;
+    private String name,phone,address;
+
+    public ContactModel(int id, String name, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
--- Kuliah/8. SQLite/app/src/main/java/com/komsi/sqlite/ListAdapter.java
+++ Kuliah/8. SQLite/app/src/main/java/com/komsi/sqlite/ListAdapter.java
@@ -1,27 +1,61 @@
 package com.komsi.sqlite;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class ListAdapter extends BaseAdapter {
+    private LayoutInflater layoutInflater;
+    private List<ContactModel> contact;
+    private Activity activity;
+
+
+    public ListAdapter(Activity activity, List<ContactModel> contact) {
+        this.activity = activity;
+        this.contact = contact;
+    }
+
     @Override
-    public int getCount() {
-        return 0;
+    public int getCount(int i) {
+        return contact.size(i);
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return contact.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        if (layoutInflater == null) {
+            layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        if (view == null)
+            view = layoutInflater.inflate(R.layout.list_row, null);
+            TextView textid, textName, textNumber, textAddress;
+            textid = view.findViewById(R.id.textId);
+            textName = view.findViewById(R.id.textName);
+            textNumber = view.findViewById(R.id.textNumber);
+            textAddress = view.findViewById(R.id.textAddress);
+
+            ContactModel data = contact.get(i);
+            textid.setText(Integer.toString(data.getId()));
+            textName.setText(data.getName());
+            textNumber.setText(data.getPhone());
+            textAddress.setText(data.getAddress());
+            return view;
+        }
     }
-}
